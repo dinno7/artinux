a82bc439ecf1a782412293ce68fb5c434b760388
dinno <tahadlrb7@gmail.com>
2026-07-18 23:30:00 +0330

    feat(domain): add Upload functionality to storage port

--- internal/domain/ports/storage.go
+++ internal/domain/ports/storage.go
@@ -3,9 +3,12 @@ package ports
 import (
 	"context"
 	"io"
+
+	"github.com/dinno7/artinux/internal/domain/entities"
 )
 
 type ObjectStorage interface {
 	Ping(ctx context.Context) error
 	CreateBucket(ctx context.Context, bucketName string, region string) error
+	Upload(ctx context.Context, reader io.Reader, artifact *entities.Artifact) (string, error)
 }
