9a5f5889acbe3e2f1494f93cbf561f45f5380def
dinno <tahadlrb7@gmail.com>
2026-07-19 08:50:40 +0330

    feat(domain): add writer like method to hasher's port

--- internal/domain/ports/checksum_hasher.go
+++ internal/domain/ports/checksum_hasher.go
@@ -3,6 +3,10 @@ package ports
 import "io"
 
 type ChecksumHasher interface {
-	ComputeFromReader(reader io.Reader) (string, error)
+	ComputeFromReaderToBase64(reader io.Reader) (string, error)
 	CompareFromReader(reader io.Reader, hashedValue string) (bool, error)
+
+	AsWriter() io.Writer
+	ComputeToBase64() string
+	Reset()
 }
