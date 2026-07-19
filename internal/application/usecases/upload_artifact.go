6f68366cde5cddff03ce6846f640e0c2ae17b110
dinno <tahadlrb7@gmail.com>
2026-07-19 12:28:21 +0330

    refactor(application): get username, hostname, os, arch from input

--- internal/application/usecases/upload_artifact.go
+++ internal/application/usecases/upload_artifact.go
@@ -9,7 +9,6 @@ import (
 	"github.com/dinno7/artinux/internal/domain/entities"
 	"github.com/dinno7/artinux/internal/domain/ports"
 	"github.com/dinno7/artinux/internal/domain/services"
-	"github.com/dinno7/artinux/pkg/helper"
 )
 
 type UploadArtifactUC struct {
@@ -33,64 +32,80 @@ func NewUploadArtifactUC(
 	}
 }
 
-func (uc *UploadArtifactUC) Execute(ctx context.Context, filePath string) (string, error) {
+type UploadArtifactInput struct {
+	FilePath string
+	Hostname string
+	Username string
+	OS       string
+	Arch     string
+}
+
+func (uc *UploadArtifactUC) Execute(
+	ctx context.Context,
+	input UploadArtifactInput,
+) (string, error) {
 	// NOTE: validate file
-	uc.logger.Info("Validate incomming file", "path", filePath)
-	validatedFile, err := uc.fileValidator.Validate(filePath)
+	uc.logger.Info("Validate incomming file", "path", input.FilePath)
+	validatedFile, err := uc.fileValidator.Validate(input.FilePath)
 	if err != nil {
-		uc.logger.Error("File validation failed", err, "path", filePath)
+		uc.logger.Error("File validation failed", err, "path", input.FilePath)
 		return "", err
 	}
+
+	makeEmptyUnknown(&input.Hostname)
+	makeEmptyUnknown(&input.Username)
+	makeEmptyUnknown(&input.OS)
+	makeEmptyUnknown(&input.Arch)
 
 	// NOTE: Create domain entity
 	uc.logger.Info("Creating domain entity")
 	artifact, err := entities.NewArtifact(
 		validatedFile.FileName,
 		validatedFile.Extension,
-		helper.GetRuntimeHostname(),
-		helper.GetRuntimeUsername(),
-		helper.GetRuntimeOS(),
-		helper.GetRuntimeArch(),
+		input.Hostname,
+		input.Username,
+		input.OS,
+		input.Arch,
 		validatedFile.FileSize,
 	)
 	if err != nil {
-		uc.logger.Error("Failed to craeting domain enity", err, "path", filePath)
+		uc.logger.Error("Failed to craeting domain enity", err, "path", input.FilePath)
 		return "", err
 	}
 
 	// NOTE: open the file for upload
-	uc.logger.Info("Opening file to upload it", "path", filePath)
+	uc.logger.Info("Opening file to upload it", "path", input.FilePath)
 	file, err := os.Open(validatedFile.FilePath)
 	if err != nil {
-		uc.logger.Error("Failed to opening file for upload", err, "path", filePath)
+		uc.logger.Error("Failed to opening file for upload", err, "path", input.FilePath)
 		return "", domain.ErrInternal.Wrap(err)
 	}
 	defer file.Close()
 
 	// NOTE: Compute local checksum
-	uc.logger.Info("Computing file checksum", "path", filePath)
+	uc.logger.Info("Computing file checksum", "path", input.FilePath)
 	localChecksum, err := uc.hasher.ComputeFromReaderToBase64(file)
 	if err != nil {
-		uc.logger.Error("Failed to compute checksum", err, "path", filePath)
+		uc.logger.Error("Failed to compute checksum", err, "path", input.FilePath)
 		return "", err
 	}
-	uc.logger.Info("Checksum computed", "path", filePath, "checksum", localChecksum)
+	uc.logger.Info("Checksum computed", "path", input.FilePath, "checksum", localChecksum)
 	artifact.AddChecksum(localChecksum)
 
 	// NOTE: for use again in hasher
 	if _, err := file.Seek(0, io.SeekStart); err != nil {
-		uc.logger.Error("Failed to reset opened file reader's pointer", err, "path", filePath)
+		uc.logger.Error("Failed to reset opened file reader's pointer", err, "path", input.FilePath)
 		return "", domain.ErrInternal.Wrap(err)
 	}
 
 	// NOTE: Upload to storage
-	uc.logger.Info("Uploading file", "path", filePath, "object_key", artifact.ObjectKey)
+	uc.logger.Info("Uploading file", "path", input.FilePath, "object_key", artifact.ObjectKey)
 	storageChecksum, err := uc.storage.Upload(ctx, file, artifact)
 	if err != nil {
 		uc.logger.Error(
 			"Failed to uploading file",
 			err,
-			"path", filePath,
+			"path", input.FilePath,
 			"object_key", artifact.ObjectKey,
 		)
 		return "", err
@@ -108,4 +123,10 @@ func (uc *UploadArtifactUC) Execute(ctx context.Context, filePath string) (strin
 	}
 
 	return artifact.ObjectKey, nil
+}
+
+func makeEmptyUnknown(inp *string) {
+	if *inp == "" {
+		*inp = "unknown"
+	}
 }
