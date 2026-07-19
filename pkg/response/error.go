63aaaf471db5b0939fadc7cef96de24df6f35f99
dinno <tahadlrb7@gmail.com>
2026-07-19 23:21:58 +0330

    feat(pkg): add http response helper

---
+++ pkg/response/error.go
@@ -0,0 +1,35 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+)
+
+// ErrorResponseData represents a standard API error response
+//
+//	@Description	Standard error response returned by the API
+type ErrorResponseData[T any] struct {
+	// HTTP status code
+	Status int `json:"status"`
+	// Error message
+	Message string `json:"message"`
+	Meta    T      `json:"meta"`
+}
+
+// ErrorResponse sends a standardized error response.
+func ErrorResponse[T any](c *echo.Context, message string, status int, meta ...T) error {
+	res := ErrorResponseData[T]{
+		Status:  status,
+		Message: message,
+	}
+	if len(meta) > 0 {
+		res.Meta = meta[0]
+	}
+	return c.JSON(status, res)
+}
+
+// BadRequestResponse sends a standardized error response with 400 code.
+func BadRequestResponse(c *echo.Context, message string) error {
+	return ErrorResponse[any](c, message, http.StatusBadRequest)
+}
---
+++ pkg/response/success.go
@@ -0,0 +1,37 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+)
+
+type ResponseSuccessData[T any] struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    T      `json:"data"`
+}
+
+// SuccessResponse sends a standardized success response
+func SuccessResponse[T any](c *echo.Context, message string, data T, status int) error {
+	return c.JSON(status, ResponseSuccessData[T]{
+		Status:  status,
+		Message: message,
+		Data:    data,
+	})
+}
+
+// OkResponse sends a standardized success response with 200 status
+func OkResponse(c *echo.Context, message string, data any) error {
+	return SuccessResponse(c, message, data, http.StatusOK)
+}
+
+// CreatedResponse sends a standardized success response with 201 status
+func CreatedResponse(c *echo.Context, message string, data any) error {
+	return SuccessResponse(c, message, data, http.StatusCreated)
+}
+
+// NoContentResponse sends a standardized success response with 204 status
+func NoContentResponse(c *echo.Context) error {
+	return c.NoContent(http.StatusNoContent)
+}
