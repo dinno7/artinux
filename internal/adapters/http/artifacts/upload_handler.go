92ec78d56d0204572d8bfea2a120db58a430f08b
dinno <tahadlrb7@gmail.com>
2026-07-23 16:46:19 +0330

    feat(adapters): handling multiple files upload

--- internal/adapters/http/artifacts/upload_handler.go
+++ internal/adapters/http/artifacts/upload_handler.go
@@ -1,17 +1,28 @@
 package artifacts
 
 import (
-	"fmt"
-	"io"
-	"math/rand/v2"
-	"mime/multipart"
-	"os"
-	"path/filepath"
-
 	"github.com/dinno7/artinux/internal/application/usecases"
 	"github.com/dinno7/artinux/pkg/response"
 	"github.com/labstack/echo/v4"
 )
+
+type UploadArtifactDto struct {
+	Arch     string `form:"arch"`
+	OS       string `form:"os"`
+	Username string `form:"username"`
+	Hostname string `form:"hostname"`
+}
+
+type UploadedArtifactsDto struct {
+	Artifacts []UploadedArtifactDto `json:"artifacts"`
+}
+
+type UploadedArtifactDto struct {
+	Ok        bool   `json:"ok"`
+	FileName  string `json:"file_name"`
+	ObjectKey string `json:"object_key,omitempty"`
+	Err       string `json:"error,omitempty"`
+}
 
 // UploadArtifact godoc
 //
@@ -25,109 +36,91 @@ import (
 //	@Param			username	formData	string	true	"Username of the uploader machine"							example(johndoe)
 //	@Param			hostname	formData	string	true	"Hostname of the source machine"							example(web-server-01)
 //	@Param			artifacts	formData	[]file	true	"Artifact file to upload (multipart)"
-//	@Success		201			{object}	response.ResponseSuccessData[map[string]string]
+//	@Success		201			{object}	response.ResponseSuccessData[UploadedArtifactsDto]
 //
 //	@Failure		400			{object}	response.ErrorResponseData[any]	"Bad request - missing required field or no files uploaded"
 //	@Failure		500			{object}	response.ErrorResponseData[any]	"Internal server error"
 //
 //	@Router			/artifacts [post]
 func (h *ArtifactHTTPHandler) UploadArtifact(c echo.Context) error {
-	arch := c.FormValue("arch")
-	osName := c.FormValue("os")
-	username := c.FormValue("username")
-	hostname := c.FormValue("hostname")
+	var payload UploadArtifactDto
+	if err := c.Bind(&payload); err != nil {
+		return err
+	}
 
-	if arch == "" {
+	if payload.Arch == "" {
 		return response.BadRequestResponse(c, "arch is required")
 	}
 
-	if osName == "" {
+	if payload.OS == "" {
 		err := response.BadRequestResponse(c, "os is required")
 		return err
 	}
 
-	if username == "" {
+	if payload.Username == "" {
 		return response.BadRequestResponse(c, "username is required")
 	}
 
-	if hostname == "" {
+	if payload.Hostname == "" {
 		return response.BadRequestResponse(c, "hostname is required")
 	}
 
 	incomingFilesHeaders, err := c.MultipartForm()
-	if err != nil {
-		return err
+	if err != nil || incomingFilesHeaders == nil {
+		return response.BadRequestResponse(c, "artifact files is required")
 	}
 
 	artifactFiles := incomingFilesHeaders.File["artifacts"]
-	if incomingFilesHeaders == nil || len(artifactFiles) == 0 {
+	if len(artifactFiles) == 0 {
 		return response.BadRequestResponse(c, "artifacts is required")
 	}
 
-	// TODO: Add batch uploading support
-	if len(artifactFiles) > 1 {
-		artifactFiles = []*multipart.FileHeader{artifactFiles[0]}
-	}
-
-	requestedFiles := []string{}
-
-	tmpDirPath := filepath.Join(
-		os.TempDir(),
-		fmt.Sprintf("artinux.%d", rand.Int32()),
-	)
-	if err := os.MkdirAll(tmpDirPath, 0o700); err != nil {
-		return err
-	}
-	defer os.RemoveAll(tmpDirPath)
-
+	// INFO: Multiple files
+	c.Logger().Info("Uploading multiple artifact")
+	items := make([]usecases.UploadArtifactItem, len(artifactFiles))
 	for i := range artifactFiles {
-		artifactFile := artifactFiles[i]
-		incomingFile, err := artifactFile.Open()
-		if err != nil {
-			return err
-		}
-
 		// TODO: Sanitize uploaded file name
-		tmpFilePath := filepath.Join(tmpDirPath, artifactFile.Filename)
-		tmpFile, err := os.OpenFile(
-			tmpFilePath,
-			os.O_CREATE|os.O_WRONLY,
-			0o600,
-		)
+		art := artifactFiles[i]
+		f, err := art.Open()
 		if err != nil {
 			return err
 		}
+		defer f.Close()
 
-		if _, err := io.Copy(tmpFile, incomingFile); err != nil {
-			c.Logger().Error("failed store incomming file", err)
-			continue
+		items[i] = usecases.UploadArtifactItem{
+			FileName:   art.Filename,
+			FileSize:   art.Size,
+			FileReader: f,
 		}
-
-		requestedFiles = append(requestedFiles, tmpFile.Name())
-		incomingFile.Close()
-		tmpFile.Close()
+	}
+	result, err := h.uploadArtifactsUC.Execute(c.Request().Context(), usecases.UploadArtifactsInput{
+		Items:    items,
+		OS:       payload.OS,
+		Arch:     payload.Arch,
+		Hostname: payload.Hostname,
+		Username: payload.Username,
+	})
+	if err != nil {
+		return response.BadRequestResponse(c, err.Error())
 	}
 
-	if len(requestedFiles) == 1 {
-		objKey, err := h.uploadArtifactUC.Execute(
-			c.Request().Context(),
-			usecases.UploadArtifactInput{
-				FilePath: requestedFiles[0],
-				OS:       osName,
-				Arch:     arch,
-				Hostname: hostname,
-				Username: username,
-			},
-		)
-		if err != nil {
-			return response.BadRequestResponse(c, err.Error())
+	finalResult := make([]UploadedArtifactsDto, 0, len(artifactFiles))
+	for _, uploadedArtifact := range result {
+		var err string
+		if uploadedArtifact.Err != nil {
+			err = uploadedArtifact.Err.Error()
 		}
-		return response.CreatedResponse(c, "Artifact uploaded successfully", map[string]string{
-			"object_key": objKey,
+		finalResult = append(finalResult, UploadedArtifactsDto{
+			Artifacts: []UploadedArtifactDto{
+				{
+					Ok:        err == "",
+					FileName:  uploadedArtifact.FileName,
+					ObjectKey: uploadedArtifact.ObjectKey,
+					Err:       err,
+				},
+			},
 		})
 	}
 
-	// TODO: Add batch uploading support
-
-	return echo.ErrInternalServerError
+	return response.CreatedResponse(c, "Artifact uploaded successfully", finalResult)
 }
